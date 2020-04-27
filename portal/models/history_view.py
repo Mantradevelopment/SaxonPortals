f084ddb7afbc2d2b50ecbb23bfbc4e9dead4aee9
kavya289 <kavya.pendyala02@gmail.com>
2020-04-27 22:52:46 +0530

    fix: try

--- portal/models/history_view.py
+++ portal/models/history_view.py
@@ -5,9 +5,9 @@ class HistoryView(db.Model):
     __bind_key__ = 'readonly'
     __tablename__ = 'CV$IF_MEM_EMP_HIS'
 
-    CLNT = db.Column(db.String(255), primary_key=True, Nullable=False)
-    MKEY = db.Column(db.String(255), primary_key=True, Nullable=False)
-    ERKEY = db.Column(db.String(255), primary_key=True, Nullable=False)
-    EMPLOYER_SNAME = db.Column(db.String(255), primary_key=True, Nullable=False)
-    EDATE = db.Column(db.DateTime, primary_key=True, Nullable=False)
-    EMP_STATUS = db.Column(db.String(255), primary_key=True, Nullable=False)
+    CLNT = db.Column(db.String(255), primary_key=True)
+    MKEY = db.Column(db.String(255), primary_key=True)
+    ERKEY = db.Column(db.String(255), primary_key=True)
+    EMPLOYER_SNAME = db.Column(db.String(255), primary_key=True)
+    EDATE = db.Column(db.DateTime, primary_key=True)
+    EMP_STATUS = db.Column(db.String(255), primary_key=True)
