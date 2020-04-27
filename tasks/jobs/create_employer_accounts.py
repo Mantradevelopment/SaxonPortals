73d7b16c326f8824c9da3b112b57671bb59dbe53
Rahman Mousavian <rahman.mousavian@oracle.com>
2020-04-27 12:35:44 +1000

    Refactored create_employer_accounts job

--- tasks/jobs/create_employer_accounts.py
+++ tasks/jobs/create_employer_accounts.py
@@ -13,24 +13,39 @@ from tasks.worker import app
 def create_employer_accounts():
     LOG.info("job:create_employer_accounts:started")
     employers = EmployerView.query.all()
-    LOG.debug("Employers fetched: %s", len(employers))
+    LOG.debug("job:create_employer_accounts: %s employers fetched", len(employers))
+
     for employer in employers:
         try:
-            # employer_valid = MemberView.query.filter(MemberView.MEMNO == employer.ERNO).scalar()
-            # if employer_valid is not None:
-            #     continue
-            user = Users(UserID=employer.ERKEY,
-                            Username=employer.ERNO,
-                            Email=employer.EMAIL,
-                            DisplayName=employer.ENAME,
-                            Role=ROLES_EMPLOYER,
-                            Status=
-                                STATUS_INACTIVE if employer.TERMDATE is not None and employer.TERMDATE < datetime.utcnow()
-                                else STATUS_ACTIVE)
-            db.session.merge(user)
-            db.session.commit()
+            _upsert_employer(employer)
         except Exception as e:
             db.session.rollback()
             LOG.warning(e)
             continue
     LOG.info("job:create_employer_accounts:done")
+
+
+def _upsert_employer(employer):
+    user = Users.query.filter_by(Username=employer.ERNO).scalar()
+    if user is None:
+        LOG.debug("job:create_employer_accounts:Employer with username '%s' does not exist. Inserting...", employer.ERNO)
+        user = Users(UserID=employer.ERKEY,
+                        Username=employer.ERNO,
+                        Email=employer.EMAIL,
+                        DisplayName=employer.ENAME,
+                        Role=ROLES_EMPLOYER,
+                        Status=_get_status(employer))
+        db.session.add(user)
+    else:
+        LOG.debug("job:create_employer_accounts:Employer with username '%s' found. Updating...", employer.ERNO)
+        user.Email = employer.EMAIL
+        user.DisplayName = employer.ENAME
+        user.Role = ROLES_EMPLOYER
+        user.Status = _get_status(employer)
+    db.session.commit()
+
+
+def _get_status(employer):
+    return STATUS_INACTIVE \
+        if (employer.TERMDATE is not None and employer.TERMDATE < datetime.utcnow()) \
+        else STATUS_ACTIVE
