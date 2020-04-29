262f03c2adb39a872f6500a177cfc841cff50d3e
kavya289 <kavya.pendyala02@gmail.com>
2020-04-29 19:25:13 +0530

    FIX: member search

--- portal/routes/accounts/search.py
+++ portal/routes/accounts/search.py
@@ -88,39 +88,69 @@ class Search(Resource):
                 if employer_ is None:
                     return {"members": []}
                 employer_sname = employer_.SNAME
-            try:
-                members = db.session.query(HistoryView, EmployerView, MemberView).filter(
-                    HistoryView.ERKEY == EmployerView.ERKEY,
-                    HistoryView.MKEY == MemberView.MKEY,
-                    EmployerView.ERKEY == employer_.ERKEY,
-                    or_(MemberView.FNAME.ilike("%" + args_dict["name"] + "%"),
-                        MemberView.LNAME.ilike("%" + args_dict["name"] + "%")),
-                    MemberView.MEMNO.ilike("%" + args_dict["ID"] + "%"),
-                    HistoryView.EMP_STATUS != "Terminated",
-                    MemberView.PSTATUS.ilike("%active%"))
+                try:
+                    members = db.session.query(HistoryView, EmployerView, MemberView).filter(
+                        HistoryView.ERKEY == EmployerView.ERKEY,
+                        HistoryView.MKEY == MemberView.MKEY,
+                        EmployerView.ERKEY == employer_.ERKEY,
+                        or_(MemberView.FNAME.ilike("%" + args_dict["name"] + "%"),
+                            MemberView.LNAME.ilike("%" + args_dict["name"] + "%")),
+                        MemberView.MEMNO.ilike("%" + args_dict["ID"] + "%"),
+                        HistoryView.EMP_STATUS != "Terminated",
+                        MemberView.PSTATUS.ilike("%active%"))
 
-                if args_dict["email"] != "" and args_dict["email"] is not None:
-                    members = members.filter(MemberView.EMAIL.ilike("%" + args_dict["email"] + "%"))
+                    if args_dict["email"] != "" and args_dict["email"] is not None:
+                        members = members.filter(MemberView.EMAIL.ilike("%" + args_dict["email"] + "%"))
 
-                if members is None:
-                    return {"members": []}
-                member_list = []
-                members = members.order_by(MemberView.MEMNO.desc()).offset(offset_).limit(50).all()
-                for his, emp, mem in members:
-                    member_list.append({
-                        'MEMNO': mem.MEMNO,
-                        'FNAME': mem.FNAME,
-                        'LNAME': mem.LNAME,
-                        'EMAIL': mem.EMAIL,
-                        'PSTATUS': mem.PSTATUS,
-                        'EM_STATUS': his.EMP_STATUS
-                    })
+                    if members is None:
+                        return {"members": []}
+                    member_list = []
+                    members = members.order_by(MemberView.MEMNO.desc()).offset(offset_).limit(50).all()
+                    for his, emp, mem in members:
+                        member_list.append({
+                            'MEMNO': mem.MEMNO,
+                            'FNAME': mem.FNAME,
+                            'LNAME': mem.LNAME,
+                            'EMAIL': mem.EMAIL,
+                            'PSTATUS': mem.PSTATUS,
+                            'EM_STATUS': his.EMP_STATUS
+                        })
 
-                return {"members": member_list}
+                    return {"members": member_list}
 
-            except Exception as e:
-                LOG.error(e)
-                raise InternalServerError("Can't retrieve members")
+                except Exception as e:
+                    LOG.error(e)
+                    raise InternalServerError("Can't retrieve members", e)
+            if employer_username == "" or employer_username is None:
+                try:
+                    members = db.session.query(HistoryView, MemberView).filter(
+                        HistoryView.MKEY == MemberView.MKEY,
+                        or_(MemberView.FNAME.ilike("%" + args_dict["name"] + "%"),
+                            MemberView.LNAME.ilike("%" + args_dict["name"] + "%")),
+                        MemberView.MEMNO.ilike("%" + args_dict["ID"] + "%"),
+                        HistoryView.EMP_STATUS != "Terminated",
+                        MemberView.PSTATUS.ilike("%active%"))
+
+                    if args_dict["email"] != "" and args_dict["email"] is not None:
+                        members = members.filter(MemberView.EMAIL.ilike("%" + args_dict["email"] + "%"))
+
+                    if members is None:
+                        return {"members": []}
+                    member_list = []
+                    members = members.order_by(MemberView.MEMNO.desc()).offset(offset_).limit(50).all()
+                    for his, mem in members:
+                        member_list.append({
+                            'MEMNO': mem.MEMNO,
+                            'FNAME': mem.FNAME,
+                            'LNAME': mem.LNAME,
+                            'EMAIL': mem.EMAIL,
+                            'PSTATUS': mem.PSTATUS,
+                            'EM_STATUS': his.EMP_STATUS
+                        })
+                    return {"members": member_list}
+                except Exception as e:
+                    LOG.error(e)
+                    raise InternalServerError("Can't retrieve members", e)
         elif search_role == roles.ROLES_EMPLOYER:
             try:
                 employers = EmployerView.query \
